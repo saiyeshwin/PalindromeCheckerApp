93a0a085e61bc57fe5136c9a7bd72d4fc0570795
saiyeshwin <saiyeshwin@gmail.com>
2026-03-03 15:55:34 +0530

    Coded UC1 to print the welcome message

---
+++ PalindromeChecker/src/com/seveneleven/palindromecheckerapp/UseCaseOnePalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+package com.seveneleven.palindromecheckerapp;
+// Use Case-01:Application Entry & Welcome Message
+// This is the entry point of the Palindrome Checker Management System
+// No palindrome logic is implemented yet
+// @author Developer
+// @version 1.0
+public class UseCaseOnePalindromeCheckerApp {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Palindrome Checker Management System");
+		System.out.println("Version 1.0");
+		System.out.println("System initialized successfully.");
+	}
+}
